85d4e16c5464732c9f43fe8c39ddcd9e5b066d26
samithspalan <samithpoojary12345@gmail.com>
2025-02-09 08:45:11 +0530

    first commit

---
+++ data/data.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        title:'Tandoori Paneer Tikka',
+        img:'',
+        para:'',
+        price:'200',
+        type:'starters',
+        id:1,
+    },
+    {
+        title:'Malai Paneer Tikka',
+        img:'',
+        para:'',
+        price:'200',
+        type:'starters',
+        id:2,
+    },
+    {
+        title:'Soya Tandoori Tikka',
+        img:'',
+        para:'',
+        price:'175',
+        type:'starters',
+        id:3,
+    },
+    {
+        title:'Tandoori Aloo',
+        img:'',
+        para:'',
+        price:'180',
+        type:'starters',
+        id:4,
+    },
+] 
+
+export default data;
